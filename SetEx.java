e4f9f9f955b33ad616097c325c2000f8bb11f8ed
Divisha Zope <157505251+Divisha78@users.noreply.github.com>
2024-02-08 10:46:30 +0530

    Create SetEx.java

---
+++ SetEx.java
@@ -0,0 +1,24 @@
+package DIV;
+
+import java.util.*;
+
+public class SetEx {
+
+	public static void main(String[] args) {
+		
+     Set<String> s=new HashSet<>();
+     s.add("Divisha");
+     s.add("Yash");
+     s.add("Sangita");
+     System.out.println(s);
+     Set<String> s1=new TreeSet<>();
+     s1.add("Divisha");
+     s1.add("yash");
+     s1.add("Sangita");
+     System.out.println(s1);
+     
+     
+     
+	}
+
+}
