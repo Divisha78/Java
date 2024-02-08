525d6e7af9c745f482eb0e2355d8d5f0d9afe547
Divisha Zope <157505251+Divisha78@users.noreply.github.com>
2024-02-08 10:41:01 +0530

    Create Forloop.java

---
+++ Forloop.java
@@ -0,0 +1,21 @@
+package DIV;
+
+public class For_loop {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++) {
+			System.out.println(i);
+		}
+		int[] a= {1,2,3};
+		for(int i=0;i<=2;i++)
+		{
+			System.out.println(a[i]);
+		}
+		for(int x:a)
+		{
+			System.out.println(x);
+		}
+
+	}
+
+	}
