567480c5a0121d2b6bcb6a2b39e0d8c27e7d13ce
Divisha Zope <157505251+Divisha78@users.noreply.github.com>
2024-02-08 10:52:54 +0530

    Create WhileLoop.java

---
+++ WhileLoop.java
@@ -0,0 +1,22 @@
+package DIV;
+
+public class While_loop {
+
+	public static void main(String[] args) {
+		int i=5;
+		while(i<10)   //5<10
+		{
+			i++;
+			if(i==8)   
+			{
+				//continue;//skip
+				continue;//stop
+			}
+			
+			System.out.println(i);
+		}
+		
+		
+
+	}
+	}
